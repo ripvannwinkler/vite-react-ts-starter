0545f67f44ff7ca4a8cf3511d6bc910996212a1a
Chris Vann <chrisvann01@gmail.com>
2023-12-29 20:48:08 -0600

    chore: add prettier options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
     },
   },
   rules: {
+    "max-len": "off",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "react/display-name": "off",
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 160,
+  singleQuote: false,
+  trailingComma: "all",
+  jsxBracketSameLine: true,
+  arrowParens: "always",
+  quoteProps: "consistent",
+  semi: true,
+};
